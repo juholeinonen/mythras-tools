953d09c2cd985d110cfb747070c06363eda614d5
Juho Leinonen <juho.leinonen@lumoa.me>
2023-07-21 11:12:11 +0300

    better visualization of different types of skills

--- bin/gui.py
+++ bin/gui.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton, QLabel, QSpinBox
+from PyQt5.QtWidgets import QGridLayout
+
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import json
@@ -26,20 +28,37 @@ class NpcWindow(QWidget):
             lbl_img.setPixmap(scaled_pixmap)
             layout.addWidget(lbl_img)
 
+        # create the grid layout
+        grid_layout = QGridLayout()
+
         # Display NPC skills
+        column_count = 3
+        total_rows = 0  # this will keep track of how many rows have been added
+
         for skill_type in ['standard_skills', 'magic_skills', 'professional_skills', 'custom_skills', 'combat_styles']:
-            lbl_skill_type = QLabel(f'{skill_type.replace("_", " ").capitalize()}: ')
-            layout.addWidget(lbl_skill_type)
-            for skill, value in self.npc_data[skill_type].items():
-                lbl_skill = QLabel(f'    {skill}: ')
+            lbl_skill_type = QLabel(f'{skill_type.replace("_", " ").capitalize()}:')
+            grid_layout.addWidget(lbl_skill_type, total_rows, 0, 1, column_count)  # span the label across all columns
+            total_rows += 1  # increment total_rows for the skill_type label
+
+            for skill_index, (skill, value) in enumerate(self.npc_data[skill_type].items()):
+                lbl_skill = QLabel(f'{skill}:')
                 spin_box = QSpinBox()
                 spin_box.setRange(1, 100)
                 spin_box.setValue(value)
-                layout.addWidget(lbl_skill)
-                layout.addWidget(spin_box)
+
+                # calculate the row and column for the grid
+                row = total_rows + skill_index // column_count
+                column = skill_index % column_count
+
+                grid_layout.addWidget(lbl_skill, row, column * 2)  # label goes in even column
+                grid_layout.addWidget(spin_box, row, column * 2 + 1)  # spinbox goes in odd column
+
+            total_rows += skill_index // column_count + 1  # increment total_rows for the number of rows of skills added
+
+        # add grid layout to the QVBoxLayout
+        layout.addLayout(grid_layout)
 
         self.setLayout(layout)
-
 
 if __name__ == '__main__':
     with open('../data/efar.json', 'r') as f:
